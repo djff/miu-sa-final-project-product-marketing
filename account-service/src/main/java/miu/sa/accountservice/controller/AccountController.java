a64992ae1082c6b8d945af19598ee9d561877321
Ajibade Eden <teejay.2005@yahoo.com>
2021-10-19 11:46:22 -0500

    modified routes for customer & affiliate

--- account-service/src/main/java/miu/sa/accountservice/controller/AccountController.java
+++ account-service/src/main/java/miu/sa/accountservice/controller/AccountController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/customer")
+@RequestMapping("api/customer/account")
 public class AccountController {
 
     private final CustomerService service;
--- account-service/src/main/java/miu/sa/accountservice/controller/AffiliateController.java
+++ account-service/src/main/java/miu/sa/accountservice/controller/AffiliateController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/affiliate")
+@RequestMapping("api/affiliate/account")
 public class AffiliateController {
 
     private final AffiliateService service;
