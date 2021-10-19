0de765ddba3ec742f773142a797c0951fe5355a9
Ajibade Eden <teejay.2005@yahoo.com>
2021-10-19 11:06:23 -0500

    initial commit

---
+++ account-service/src/main/java/miu/sa/accountservice/repository/AffiliateRepository.java
@@ -0,0 +1,15 @@
+package miu.sa.accountservice.repository;
+
+import miu.sa.accountservice.model.entity.Affiliate;
+import miu.sa.accountservice.model.entity.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AffiliateRepository extends MongoRepository<Affiliate, UUID> {
+    Optional<Affiliate> findByEmailAndPassword(String email, String password);
+    Optional<Affiliate> findByEmail(String email);
+}
