7d446b0762e79f91f26a04da491f0d15f42d0246
Ajibade Eden <teejay.2005@yahoo.com>
2021-10-22 10:33:41 -0500

    fix: gateway-service routes

--- gateway-service/src/main/java/com/miusaatega/gatewayservice/config/JwtAuthenticationFilter.java
+++ gateway-service/src/main/java/com/miusaatega/gatewayservice/config/JwtAuthenticationFilter.java
@@ -35,7 +35,7 @@ public class JwtAuthenticationFilter implements GatewayFilter {
 
         ServerHttpRequest request = (ServerHttpRequest) exchange.getRequest();
 
-        final List<String> apiEndpoints = List.of("/register", "/login", "/orders", "/payments", "/products", "api/affiliates/productMarkup");
+        final List<String> apiEndpoints = List.of("/register", "/login", "/orders", "/payments", "/products", "/api/affiliates/productMarkup", "/api/affiliates/transaction");
 
         Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
                 .noneMatch(uri -> r.getURI().getPath().contains(uri));
